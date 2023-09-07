8c55bff49b8735960d7a4dbefccd898438b011eb
Allen Kim <avalmercafe02@gmail.com>
2023-09-07 01:14:02 -0400

    filtered grids

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -5,11 +5,22 @@ defmodule Identicon do
     |> hash_input
     |> pick_color
     |> build_grid
+    |> filter_odd_squares
+  end
+  def filter_odd_squares(%Identicon.Image{grid: grid} = image ) do
+    grid = Enum.filter grid, fn({code, _index} = square) ->
+      rem(code, 2) == 0
+    end
+
+  %Identicon.Image{image | grid: grid}
   end
   def build_grid(%Identicon.Image{hex: hex} = image) do
-    hex
+    grid = hex
     |> Enum.chunk(3)
     |> Enum.map(&mirror_row/1)
+    |> List.flatten
+    |> Enum.with_index
+    %Identicon.Image{image | grid: grid}
   end
   def mirror_row(row) do
     [first,second | _tail] = row
